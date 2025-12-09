9aeefad3fa39a7f41a4526eeb14c6a750b5b34bf
sinemypolat <sinemypolat@gmail.com>
2025-12-09 21:39:43 +0000

    fix: day 9

--- day09/solution.py
+++ day09/solution.py
@@ -37,38 +37,43 @@ def part2(input_data):
     perimeter = set()
 
     for i in range(n):
-        x1, y1 = input_data[i - 1]
-        x2, y2 = input_data[i]
+        x1, y1 = input_data[i]
+        x2, y2 = input_data[(i + 1) % n]
 
-        if x1 == x2:  # vertical edge
+        if y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                perimeter.add((x, y1))
+        else:
             for y in range(min(y1, y2), max(y1, y2) + 1):
                 perimeter.add((x1, y))
 
-        elif y1 == y2:  # horizontal edge
-            for x in range(min(x1, x2), max(x1, x2) + 1):
-                perimeter.add((x, y1))
+    max_area = 0
 
-    result = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            x1, y1 = input_data[i]
+            x2, y2 = input_data[j]
 
-    for area, (i, j) in areas:
-        x_min = min(input_data[i][0], input_data[j][0])
-        x_max = max(input_data[i][0], input_data[j][0])
-        y_min = min(input_data[i][1], input_data[j][1])
-        y_max = max(input_data[i][1], input_data[j][1])
+            area = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
 
-        bad = False
-        for x, y in perimeter:
-            if x_min < x < x_max and y_min < y < y_max:
-                bad = True
-                break
+            if area <= max_area:
+                continue
 
-        if bad:
-            continue
+            min_x, max_x = min(x1, x2), max(x1, x2)
+            min_y, max_y = min(y1, y2), max(y1, y2)
 
-        result = area
-        break
+            # Check if any perimeter point is strictly inside the rectangle
+            found_conflict = False
+            for px, py in perimeter:
+                if min_x < px < max_x and min_y < py < max_y:
+                    found_conflict = True
+                    break
 
-    return result
+            # Rectangle is valid if no perimeter points are strictly inside
+            if not found_conflict:
+                max_area = area
+
+    return max_area
 
 
 def main():
